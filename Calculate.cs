6a8c9d503518c989574c94c931349c8c4ddef26f
unknown <abhisheksrivastava9719@gmail.com>
2022-06-21 22:49:36 +0530

    M/c gives change in least notes

---
+++ Calculate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartTwo
+{
+    internal class Calculate
+    {
+        static int[] notes = new int[] { 2000, 500, 200, 100, 50, 20, 10, 5, 2, 1 };
+        static int l = notes.Length;
+        public static void chang(int n,int i)
+        {
+            int num_of_notes = n/notes[i];                              //gets number of notes
+            if (num_of_notes > 0)
+            {
+                Console.WriteLine(num_of_notes + " note of " + notes[i]);
+                if ((n - (num_of_notes * notes[i]))!=0)                  //exercises recursion if left change is not equal to zero
+                {
+                    chang((n - (num_of_notes * notes[i])), i + 1);
+                }
+            }
+            else 
+            {
+                chang(n, i + 1);                                    //if current note is greater than the change left
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PartTwo;
+Console.WriteLine("Enter the amount of the change without decimal");
+int change=Convert.ToInt32(Console.ReadLine());
+int i = 0;                          //index for array
+Calculate.chang(change, i);         //calling  static function
