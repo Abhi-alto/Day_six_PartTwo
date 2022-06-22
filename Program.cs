975deb714dddde19c387788ebc53eb4262554173
unknown <abhisheksrivastava9719@gmail.com>
2022-06-22 06:36:33 +0530

    Day for the date

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PartTwo;
+Console.WriteLine("Welcome to the day from date program");
+Console.WriteLine("Enter day of the month dd");
+int d = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the month mm");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the year yyyy");
+int y = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[] { 31,28,31,30,31,30,31,31,30,31,30,31};
+if(y%400==0||(y%100!=0&&y%4==0))                        //If year is 1600 or the year is 1500 for leap year
+{
+    arr[1] = 29;
+}
+if (m >= 1 && m <=12 || y > 0 || d > 0 && d <= arr[m-1])
+{
+    Util.date(d,m,y);
+}
+else
+{
+    Console.WriteLine("Wrong input");
+
+}
---
+++ util.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartTwo
+{
+    internal class Util
+    {
+         public static void date(int d, int m, int y)
+        {
+            int yy = y - ((14 - m) / 12);
+            int x = yy + yy / 4 + yy / 100 + yy / 400;
+            int mm = m + 12 * ((14 - m) / 12) - 2;
+            int dd = (d + x + ( 31*mm / 12)) % 7;
+            Console.WriteLine(dd);
+        }
+    }
+}
+
+
